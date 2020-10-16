9b9e7ce26ee9af375e5b6ab7a6c99904b40edf5d
Chris C <xstos@users.noreply.github.com>
2020-10-15 20:22:55 -0400

    basic texture atlas

--- pixi/src/App.tsx
+++ pixi/src/App.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import * as React from 'react';
 import * as PIXI from 'pixi.js'
+import KeyboardEventHandler from 'react-keyboard-event-handler';
+
 //import {OutlineFilter} from '@pixi/filter-outline'
 export class App extends React.Component {
   el: HTMLDivElement;
@@ -7,6 +10,8 @@ export class App extends React.Component {
   pixi: PIXI.Application;
   renderer: PIXI.Renderer;
   requestID: number;
+  keyboardCallback: (key) => void
+  jiggle: () => void;
   constructor(props) {
     super(props);
     this.jiggle = ()=>{}
@@ -25,14 +30,14 @@ export class App extends React.Component {
   handleWindowResize = () => {
     if (!this.pixi) return;
     this.clientRect = this.el.getBoundingClientRect()
-    var {width, height} = this.clientRect
+    const {width, height} = this.clientRect
     //https://stackoverflow.com/questions/30554533/dynamically-resize-the-pixi-stage-and-its-contents-on-window-resize-and-window
     this.pixi.resize();
     //this.camera.aspect = width / height;
     //this.camera.updateProjectionMatrix();
   }
   sceneSetup = () => {
-    var {width, height} = this.clientRect
+    const {width, height} = this.clientRect
     const pixi = new PIXI.Application({width, height, resizeTo: this.el})
 
     this.renderer = pixi.renderer;
@@ -40,52 +45,78 @@ export class App extends React.Component {
 
     this.el.appendChild(this.pixi.view);
 
-    let style = new PIXI.TextStyle({fontFamily : 'Consolas', fontSize: 32, fill : 0xffffff, align : 'center'})
-    let textMetrics = PIXI.TextMetrics.measureText('a', style)
-    var texture = PIXI.RenderTexture.create({ width: textMetrics.width, height: textMetrics.height });
-    const textA = new PIXI.Text('a', style);
-    pixi.renderer.render(textA,texture);
-    // var r1 = new PIXI.Graphics();
-    // r1.beginFill(0x00ffff);
-    // r1.drawRect(0, 0, 100, 100);
-    // r1.endFill();
-    function makeLetter(x: number = 100, y: number = 100) {
-      var block = new PIXI.Sprite(texture);
-      block.scale.x = 0.5
-      block.scale.y = 0.5
-      block.position.x = x;
-      block.position.y = y;
-      block.anchor.x = 1;
-      block.anchor.y = 1;
-      return block;
+    const style = new PIXI.TextStyle({fontFamily : 'Consolas', fontSize: 16, fill : 0xffffff, align : 'center'})
+    const textMetrics = PIXI.TextMetrics.measureText('a', style)
+    const letterAtlas = LetterAtlas()
+
+    function LetterAtlas() {
+      const textures = []
+      return function (letter: string) {
+        const index = letter.charCodeAt(0);
+        let ret = textures[index]
+        if (ret) return ret;
+        ret = RenderText(letter)
+        textures[index] = ret
+        return ret
+      }
     }
 
-// pixi.renderer.render(r1,texture);
-    makeLetter();
+    function RenderText(text): PIXI.RenderTexture {
+      const texture = PIXI.RenderTexture.create();
+      const t = new PIXI.Text(text, style);
+      pixi.renderer.render(t, texture);
+      return texture;
+    }
+
+    function Rect(x,y,w,h) {
+      const r1 = new PIXI.Graphics();
+      r1.beginFill(0x00ffff);
+      r1.drawRect(x, y, w, h);
+      r1.endFill();
+      //pixi.renderer.render(r1,texture);
+    }
+
+    function makeLetterSprite(letter) {
+      const sprite = new PIXI.Sprite(letterAtlas(letter));
+      //sprite.scale.x = 1
+      //sprite.scale.y = 1
+      //sprite.position.x = x;
+      //sprite.position.y = y;
+      sprite.anchor.x = 0;
+      sprite.anchor.y = 0;
+      return sprite;
+    }
 
     //block.filters= [new OutlineFilter(2, 0x99ff99)];
 
-    var sprites = new PIXI.ParticleContainer(20000, {
-      position: true,
-    });
-    var nx = div(width, textMetrics.width/2)
-    var ny = div(height, textMetrics.height/2)
-    var derp = []
-    for (let i = 0; i < nx; i++) {
-      for (let j = 0; j < ny; j++) {
-        var l = makeLetter(i*textMetrics.width/2, j*textMetrics.height/2)
-        derp.push(l)
-        sprites.addChild(l)
+    //var sprites = new PIXI.ParticleContainer(20000, { position: true, });
+    const spriteContainer = new PIXI.Container();
+    const nx = div(width, textMetrics.width)
+    const ny = div(height, textMetrics.height)
+    const spriteList = []
+
+    function render() {
+      for (let i = 0; i < spriteList.length; i++) {
+        const item = spriteList[i]
+        item.position.x = i*textMetrics.width
       }
     }
-    pixi.stage.addChild(sprites);
+
+    pixi.stage.addChild(spriteContainer);
     this.jiggle = function () {
-      for (const elm of derp) {
-        const derpElement = elm.position
-        derpElement.x = derpElement.x + (Math.random()-0.5) * 5;
-        derpElement.y = derpElement.y+ (Math.random()-0.5)*5;
+      for (const elm of spriteList) {
+        const pos = elm.position
+        pos.x = pos.x + (Math.random()-0.5) * 5;
+        pos.y = pos.y + (Math.random()-0.5) * 5;
       }
       //this.renderer.render();
+    }
+    this.keyboardCallback = function (key) {
+      console.log(key)
+      const letter = makeLetterSprite(key)
+      spriteList.push(letter)
+      spriteContainer.addChild(letter)
+      render()
     }
   };
   addCustomSceneObjects = () => {
@@ -94,13 +125,18 @@ export class App extends React.Component {
   startAnimationLoop = () => {
     // this.cube.rotation.x += 0.01;
     // this.cube.rotation.y += 0.01;
-    this.jiggle()
+    //this.jiggle()
+
     this.requestID = window.requestAnimationFrame(this.startAnimationLoop);
   };
-  private jiggle: () => void;
 
   render() {
-    return <div className={"appdiv"} ref={ref => (this.el = ref)} />
+    return <div className={"appdiv"} ref={ref => (this.el = ref)}>
+      <KeyboardEventHandler
+          handleKeys={['alphanumeric']}
+          onKeyEvent={(key)=> this.keyboardCallback(key)}>
+      </KeyboardEventHandler>
+    </div>
   }
 }
 function div(a, by) {
